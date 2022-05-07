b179fdb5f60ae7ac11770bc1173b616be8db85ed
Lucas <lac.lucas96@gmail.com>
2022-05-06 22:52:42 -0300

    Create banner

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
